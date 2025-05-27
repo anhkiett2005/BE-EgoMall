a75a62fa92f6aeba755f667e23679d9a38c2dd41
TrinhQuangThang <thangtqps39500@gmail.com>
2025-05-28 00:31:56 +0700

    thangnek

---
+++ database/migrations/2025_05_27_134236_create_blog_contents_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blog_contents', function (Blueprint $table) {
+            $table->id(); // Primary key
+            $table->foreignId('blog_id')->constrained('blogs'); // FK đến blogs
+            $table->integer('order')->default(0); // Thứ tự hiển thị
+            $table->string('title')->nullable(); // Tiêu đề của đoạn nội dung
+            $table->text('content'); // Nội dung chi tiết
+            $table->timestamps(); // created_at & updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blog_contents');
+    }
+};
---
+++ database/migrations/2025_05_27_142010_create_coupons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code');
+            $table->string('description');
+            $table->string('discount_type');
+            $table->string('discount_value');
+            $table->integer('min_order_value');
+            $table->integer('usage_limit');
+            $table->integer('used_count')->default(0);
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
