ba8e6c2a905b16d35fe9d8285c07c366806d94d8
Chau Anh Kiet <kietcaps39286@gmail.com>
2025-05-27 21:49:11 +0700

    push migration roles, role_has_permissions, permissions, system_settings

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,9 +15,18 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->text('phone');
+            $table->string('google_id')->nullable();
+            $table->string('facebook_id')->nullable();
+            $table->enum('provider', ['google', 'facebook'])->nullable();
+            $table->string('address');
+            $table->string('image');
+            $table->foreignId('role_id')->references('id')->on('roles');
+            $table->boolean('is_active')->default(0);
+            $table->timestamp('email_verified_at');
+            $table->string('otp')->nullable();
+            $table->timestamp('expries_at')->nullable();
             $table->timestamps();
         });
 
@@ -25,15 +34,6 @@ return new class extends Migration
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
         });
     }
 
