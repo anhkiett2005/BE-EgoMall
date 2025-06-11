84013b2911dee3db23eb10796b92706df45066fd
Chau Anh Kiet <kietcaps39286@gmail.com>
2025-06-11 23:08:48 +0700

    remove field provider in migration user table

--- database/migrations/2025_06_04_182642_create_users_table.php
+++ database/migrations/2025_06_04_182642_create_users_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('phone')->nullable()->comment('Số điện thoại người dùng');
             $table->string('google_id')->nullable()->comment('Lưu Google OAuth ID nếu đăng nhập bằng Google');
             $table->string('facebook_id')->nullable()->comment('Lưu Facebook OAuth ID nếu đăng nhập bằng Facebook');
-            $table->string('provider')->nullable()->comment('Loại provider khi đăng nhập (google, facebook, ...)');
             $table->text('address')->nullable()->comment('Địa chỉ người dùng');
             $table->string('image')->nullable()->comment('URL hoặc đường dẫn avatar người dùng');
 
