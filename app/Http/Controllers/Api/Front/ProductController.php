90145ff8688b1412b773efd0d6a0bb0960eba8f8
Chau Anh Kiet <kietcaps39286@gmail.com>
2025-06-26 02:19:05 +0700

    done api product all and filter by request

--- app/Http/Controllers/Api/Front/ProductController.php
+++ app/Http/Controllers/Api/Front/ProductController.php
@@ -8,6 +8,7 @@ use App\Models\Product;
 use App\Models\Promotion;
 use App\Response\ApiResponse;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -17,21 +18,90 @@ class ProductController extends Controller
      */
     public function index()
     {
-        // lấy product and các vairant và đánh giá trung bình review về sản phẩm này
-        $products = Product::with([
-            'category',
-            'brand',
-            'variants' => function($query) {
-                     $query->where('is_active', '!=', 0)
-                           ->with([
+        // lấy product and các vairant và đánh giá trung bình review về sản phẩm này and filter
+        $request = request();
+        $sub = DB::table('product_variants')
+                 ->selectRaw('MAX(COALESCE(sale_price, price))')
+                 ->whereColumn('product_variants.product_id', 'products.id');
+
+        $query = Product::query()
+                ->select('*')
+                ->selectSub($sub, 'max_price')
+                ->with([
+                    'category',
+                    'brand',
+                    'variants' => function($query) {
+                        $query->where('is_active', '!=', 0)
+                            ->with([
                                 'images',
                                 'values.variantValue.option',
                                 'orderDetails.order.review'
                             ]);
-            }
-        ])
-        ->where('is_active', '!=', 0)
-        ->get();
+                    }
+                ])
+                ->where('is_active', '!=', 0);
+
+        // Lọc theo category
+        if($request->has('category')) {
+            $query->where('category_id', '=', $request->category);
+        }
+
+        // Lọc theo brand (nếu có từ fe)
+        if($request->has('brand')) {
+            $query->where('brand_id', '=', $request->brand);
+        }
+
+        // Lọc theo loại da
+        if($request->has('type_skin')) {
+            $query->where('type_skin', '=', $request->type_skin);
+        }
+
+        // Lọc theo loại sản phẩm
+        if ($request->filled('type')) {
+            $type = $request->type;
+
+            $query->where(function ($q) use ($type) {
+                // Sản phẩm có tên giống từ khoá
+                $q->where('name', 'like', '%' . $type . '%')
+
+                // Hoặc thuộc danh mục (cha hoặc con) có tên giống từ khoá
+                ->orWhereHas('category', function ($catQ) use ($type) {
+                    $catQ->where('name', 'like', '%' . $type . '%');
+                });
+            });
+        }
+
+        // lọc theo mức giá từ min-> max
+        if($request->filled('price_range')) {
+            //Xử lý tách giá min và max
+            [$min, $max] = array_map('intval',explode('-', $request->price_range));
+            $query->whereHas('variants', function ($q) use($min, $max) {
+               $q->where(function($subQ) use($min, $max) {
+                   $subQ->where(function($hasSalePriceQ) use($min, $max) {
+                        // nếu có sale_price  thì lọc theo giá sale vì variant áp dụng theo giá sale
+                        $hasSalePriceQ->whereNotNull('sale_price')
+                                      ->whereBetween('sale_price', [$min, $max]);
+                   })
+                   ->orWhere(function($noSalePriceQ) use($min, $max) {
+                        // nếu sale_price null thì lọc theo price
+                        $noSalePriceQ->whereNull('sale_price')
+                                     ->whereBetween('price', [$min, $max]);
+                   });
+               });
+            });
+        }
+
+        // Sort theo các tiêu chí
+        if($request->has('sort')) {
+            $this->sortProduct($request, $query);
+        }
+
+
+
+        // xử lý dữ liệu và trả về
+        $products = $query->get();
+
+
 
         $productLists = $products->map(function($product): array {
             $allReviews = collect();
@@ -75,7 +145,7 @@ class ProductController extends Controller
             ];
         });
 
-        return ApiResponse::success('Data fetched successfully', data: $productLists);
+        return ApiResponse::success('Lấy danh sách sản phẩm thành công!!', data: $productLists);
     }
 
 
@@ -223,6 +293,32 @@ class ProductController extends Controller
         ]);
 
         return ApiResponse::success('Data fetched successfully', data: $listDetails);
+    }
+
+
+    private function sortProduct($request, $query)
+    {
+        switch ($request->sort) {
+             case 'a-z':
+                $query->orderBy('id', 'asc');
+                break;
+            case 'z-a':
+                $query->orderBy('id', 'desc');
+                break;
+             case 'price_asc':
+                $query->orderBy('max_price', 'asc'); // Đã có sẵn từ selectSub
+                break;
+            case 'price_desc':
+                $query->orderBy('max_price', 'desc'); // Đã có sẵn từ selectSub
+                 break;
+            case 'new':
+                $query->where('created_at', '>=', now()->subDays(7)) // lọc hàng mới 7 ngày gần đây
+                      ->orderBy('created_at', 'desc');
+                break;
+            default:
+                $query->orderBy('created_at', 'desc');
+                break;
+        }
     }
 
 }
