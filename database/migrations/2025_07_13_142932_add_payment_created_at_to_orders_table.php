ae9295767270ed96ad4ee96883e35ca117a05270
Chau Anh Kiet <kietcaps39286@gmail.com>
2025-07-13 14:32:23 +0700

    update

--- database/migrations/2025_07_13_142932_add_payment_created_at_to_orders_table.php
+++ database/migrations/2025_07_13_142932_add_payment_created_at_to_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->timestamp('payment_created_at')->nullable();
+            $table->timestamp('payment_created_at')->after('payment_method')->nullable();
         });
     }
 
