3f12e10b18e70db468c3f08669cf212c725c9e65
Chau Anh Kiet <kietcaps39286@gmail.com>
2025-07-13 12:43:32 +0700

    add updated_at to table orders

--- database/migrations/2025_05_27_142852_create_orders_table.php
+++ database/migrations/2025_05_27_142852_create_orders_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('unique_id')->unique();
             $table->foreignId('user_id')->constrained('users');
             $table->integer('total_price');
             $table->enum('status', ['ordered', 'confirmed', 'shipping', 'delivered','cancelled','return_sales'])->default('ordered');
@@ -26,7 +27,7 @@ return new class extends Migration
             $table->timestamp('payment_date')->nullable();
             $table->string('transaction_id')->nullable();
             $table->foreignId('coupon_id')->nullable()->references('id')->on('coupons')->nullOnDelete();
-            $table->timestamp('created_at');
+            $table->timestamps();
         });
     }
 
