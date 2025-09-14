cfc64dc7e515d18f768f9078e735d5d87e01b09a
Chau Anh Kiet <kietcaps39286@gmail.com>
2025-09-14 22:14:30 +0700

    update source code

--- app/Models/District.php
+++ app/Models/District.php
@@ -3,24 +3,35 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class District extends Model
 {
-    use HasFactory;
+    protected $table = 'districts';
+    protected $primaryKey = 'code';
+    public $incrementing = false;
+    protected $keyType = 'string';
+    public $timestamps = false;
 
     protected $fillable = [
+        'code',
         'name',
-        'province_id',
+        'name_en',
+        'full_name',
+        'full_name_en',
+        'code_name',
+        'province_code',
+        'administrative_unit_id',
     ];
 
+    // Quan hệ n-1 với Province
     public function province()
     {
-        return $this->belongsTo(Province::class);
+        return $this->belongsTo(Province::class, 'province_code', 'code');
     }
 
+    // Quan hệ 1-n với Ward
     public function wards()
     {
-        return $this->hasMany(Ward::class);
+        return $this->hasMany(Ward::class, 'district_code', 'code');
     }
 }
